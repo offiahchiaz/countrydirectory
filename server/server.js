ea066c51b27b8cdc03bdec4fcc3ed257e8c6a9d4
Offiahchiaz <offiahchiaz@gmail.com>
2018-12-24 23:50:53 +0100

    Add functionality for consuming external api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const express = require('express');
+const request = require('request');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.static(publicPath));
+
+request.get("https://restcountries.eu/rest/v2/name/nigeria", function (err, res, body) {
+    if (err && response.statusCode === 404) {
+        return console.log(err);
+    }
+
+    console.log(JSON.parse(body));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
