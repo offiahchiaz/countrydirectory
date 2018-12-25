0f04577939b3efb44804c3671cbf87bd9877a406
Offiahchiaz <offiahchiaz@gmail.com>
2018-12-25 10:05:07 +0100

    Add ejs templating engine

--- server/server.js
+++ server/server.js
@@ -2,19 +2,26 @@ const path = require('path');
 const express = require('express');
 const request = require('request');
 
-const publicPath = path.join(__dirname, '../public');
+//const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 4000;
-const app = express();
+const app = express();    
+ 
+app.set('view engine', 'ejs');   
 
-app.use(express.static(publicPath));
-
-request.get("https://restcountries.eu/rest/v2/name/nigeria", function (err, res, body) {
-    if (err && response.statusCode === 404) {
-        return console.log(err);
-    }
-
-    console.log(JSON.parse(body));
+app.get('/', function (req, res) {   
+ 
+    const url = 'https://restcountries.eu/rest/v2/all';
+    request(url, function (err, response, body) { 
+        if (err) {
+            res.render('index', {data: null, error: 'Error, please try again'});
+        } else {
+            let data = JSON.parse(body);
+            res.render('index', {data: data, error: null}); 
+        }
+    });  
 });
+
+//app.use(express.static(publicPath));
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
