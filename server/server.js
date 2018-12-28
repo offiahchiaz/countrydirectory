104f26611d1afc6ebe97178bcfd36967493d9416
Offiahchiaz <offiahchiaz@gmail.com>
2018-12-28 03:10:22 +0100

    Add /:name route and also country.ejs for displaying details of a country

--- server/server.js
+++ server/server.js
@@ -4,12 +4,12 @@ const request = require('request');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 4000; 
-const app = express();      
+const app = express();       
  
 app.use(express.static(publicPath));
 app.set('view engine', 'ejs');        
   
-app.get('/', function (req, res) {     
+app.get('/', function (req, res) {    
  
     const url = 'https://restcountries.eu/rest/v2/all';
     request(url, function (err, response, body) { 
@@ -18,10 +18,25 @@ app.get('/', function (req, res) {
         } else {
             let data = JSON.parse(body);
             res.render('index', {data: data, error: null}); 
+            console.log(data.length) //250;
         }
     });  
 });
  
+app.get('/:name', function (req, res) {
+    let countryName = req.params.name;
+    let url = `https://restcountries.eu/rest/v2/name/${countryName}`
+    request(url, function (err, response, body) {
+        if (err) {
+            res.render('country', {data: null, error: 'Error, please try again'});
+        } else {
+            let data = JSON.parse(body);
+            res.render('country', {data: data, error: null}); 
+            console.log(data.length) //250;
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
