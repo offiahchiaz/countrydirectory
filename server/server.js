cb4fe5e63f3b7aad61c592399cb5c38900f755ac
Offiahchiaz <offiahchiaz@gmail.com>
2018-12-31 00:03:14 +0100

    Minor changes

--- server/server.js
+++ server/server.js
@@ -18,13 +18,13 @@ app.get('/', function (req, res) {
         } else {
             let data = JSON.parse(body);
             res.render('index', {data: data, error: null}); 
-            //console.log('Number of countries', data.length); //250;
         }
     });  
 });
  
 app.get('/:name', function (req, res) {
-    let countryName = req.params.name;
+    let countryName = req.params.name; 
+    console.log(countryName)
     let url = `https://restcountries.eu/rest/v2/name/${countryName}`
     request(url, function (err, response, body) {
         if (err) {
@@ -32,10 +32,23 @@ app.get('/:name', function (req, res) {
         } else {
             let data = JSON.parse(body);
             res.render('country', {data: data, error: null}); 
-            //console.log('Number of country', data.length)
         }
     });
 });
+
+app.get('/:region', function (req, res) {
+    let regionName = req.params.region;
+    console.log(regionName)
+    let url = `https://restcountries.eu/rest/v2/region/${regionName}`;
+    request(url, function (err, response, body) {
+        if (err) {
+            res.render('region', {data: null, error: 'Error, please try again'});
+        } else {
+            let data = JSON.parse(body);
+            res.render('region', {data: data, error: null}); 
+        }
+    });
+}); 
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
